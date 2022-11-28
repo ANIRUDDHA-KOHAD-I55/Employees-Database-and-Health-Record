9a21e249ebb54fe78d056a8ee01e1d1269d2851e
AnirK <97038875+ANIRUDDHA-KOHAD-I55@users.noreply.github.com>
2022-11-28 15:45:24 +0530

    Create Raspberry_pi_c_code_for_bluetooth_device.c

---
+++ Raspberry_pi_c_code_for_bluetooth_device.c
@@ -0,0 +1,29 @@
+import pygatt
+from binascii import hexlify
+import time
+adapter = pygatt.GATTToolBackend()
+
+def handle_data(handle, value):
+    """
+    handle -- integer, characteristic read handle the data was received on
+    value -- bytearray, the data returned in the notification
+    """
+    print("Received data: %s" % hexlify(value))
+
+try:
+    adapter.start()
+    device = adapter.connect('AC:23:3F:AA:36:7C')
+
+    device.subscribe("7f280002-8204-f393-e0a9-e50e24dcca9e",
+                     callback=handle_data)
+
+    # The subscription runs on a background thread. You must stop this main
+    # thread from exiting, otherwise you will not receive any messages, and
+    # the program will exit. Sleeping in a while loop like this is a simple
+    # solution that won't eat up unnecessary CPU, but there are many other
+    # ways to handle this in more complicated program. Multi-threaded
+    # programming is outside the scope of this README.
+    while True:
+        time.sleep(10)
+finally:
+    adapter.stop()
